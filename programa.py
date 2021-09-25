0c621876a12c7a5fc6514ee73886ad79470796ef
XaviiSW <91351687+XaviiSW@users.noreply.github.com>
2021-09-25 09:15:12 -0300

    Add files via upload

---
+++ programa.py
@@ -0,0 +1,13 @@
+print("----------------------------")
+print("------Creditos Mendell------")
+print("----------------------------")
+
+varsNombre = input("Ingrese su nombre: ")
+varsApellido = input("Ingrese su Apellido: ")
+varsEdad = input("Ingrese su edad: ")
+varsMontoSolicitud = input("Ingrese su Monto Credito: ")
+varsSalaraio = input("Ingrese su sueldo mensual: " ) 
+
+if varsMontoSolicitud > varsSalaraio
+    print("Credito supera su sueldo")
+
